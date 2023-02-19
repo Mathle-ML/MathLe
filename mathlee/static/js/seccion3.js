97a1e7e380bb18fed19628913703652cc5292c92
Nestor <nestor.ascencio1418@alumnos.udg.mx>
2023-02-18 20:37:23 -0600

    pre index

--- mathlee/static/js/seccion3.js
+++ mathlee/static/js/seccion3.js
@@ -12,24 +12,30 @@ const cont_4 = document.getElementById("temas-4");*/
 
 const div_elements = document.getElementsByClassName("temas");
 const cb_elements = document.getElementsByClassName("cbTopic");
-const xd = document.getElementById("temas-2");
-
-console.log(div_elements);
-console.log(cb_elements);
 
 for(var i = 0; i<div_elements.length; i++){
     div_elements.item(i).style.visibility = 'hidden';
 }
 
 for(var i = 0; i<cb_elements.length; i++){
+    cb_elements.item(i).setAttribute("number", i);
     cb_elements.item(i).addEventListener('change', e => {
         if(e.target.checked === true) {
-            xd.style.visibility = 'visible';
-            console.log("is checked");
+            for(var o = 0; o<div_elements.length;o++){
+                if(!(cb_elements.item(o) === e.target)){
+                    div_elements.item(o).style.transition = 'none';
+                    cb_elements.item(o).checked = false;
+                    div_elements.item(o).style.visibility = 'hidden';
+                    div_elements.item(o).style.transition = 'height .2s, background-color .2s';
+                    console.log(cb_elements.item(o).checked);
+                }
+            }
+            div_elements.item(e.target.getAttribute("number")).style.visibility = 'visible';
         }
         if(e.target.checked === false) {
-            xd.style.visibility = 'hidden';
-            console.log("no");
+            setTimeout(() => {
+                div_elements.item(e.target.getAttribute("number")).style.visibility = 'hidden';
+            }, 200);
        }
     });
 }
