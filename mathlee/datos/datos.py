faa6bec0cef18d4cd5cf2024d54880d46b0a5660
NestorNey <98431315+NestorNey@users.noreply.github.com>
2023-04-22 16:10:31 -0600

    ya lo subi xd (#9)

---
+++ mathlee/datos/datos.py
@@ -0,0 +1,335 @@
+data = {
+    'nivel_1' : {
+        'id' : 'nivel_1',
+        'text' : 'Nivel 1 - 2° y 3° de primaria',
+        'temas' : {
+            'tema_1' : {
+                'id' : 'tema_1',
+                'text' : 'Las sumas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'La loca de los gatos'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Tetris de sumas'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_2' : {
+                'id' : 'tema_2',
+                'text' : 'Las Restas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_3' : {
+                'id' : 'tema_3',
+                'text' : 'Tamaños y medidas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_4' : {
+                'id' : 'tema_4',
+                'text' : 'Figuras geometricas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_5' : {
+                'id' : 'tema_5',
+                'text' : 'Fracciones',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_6' : {
+                'id' : 'tema_6',
+                'text' : 'Multiplicacion',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_7' : {
+                'id' : 'tema_7',
+                'text' : 'Divicion',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_8' : {
+                'id' : 'tema_8',
+                'text' : 'Fracciones II',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_9' : {
+                'id' : 'tema_9',
+                'text' : 'Patrones numericos',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_10' : {
+                'id' : 'tema_10',
+                'text' : 'Magnitudes',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            },
+            'tema_11' : {
+                'id' : 'tema_11',
+                'text' : 'Pictogramas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'No me gusta la pizza'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Ya no quiero mas pizza'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            }
+        }
+    },
+    'nivel_2' : {
+        'id' : 'nivel_2',
+        'text' : 'Nivel 2 - 4° y 5° de primaria',
+        'temas' : {
+            'tema_1' : {
+                'id' : 'tema_1',
+                'text' : 'Las sumas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'La loca de los gatos'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Tetris de sumas'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            }
+        }
+    },
+    'nivel_3' : {
+        'id' : 'nivel_3',
+        'text' : 'Nivel 3 - 6° de primaria y 1° de secundaria',
+        'temas' : {
+            'tema_1' : {
+                'id' : 'tema_1',
+                'text' : 'Las sumas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'La loca de los gatos'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Tetris de sumas'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            }
+        }
+    },
+    'nivel_4' : {
+        'id' : 'nivel_4',
+        'text' : 'Nivel 4 - 2° y 3° de secundaria',
+        'temas' : {
+            'tema_1' : {
+                'id' : 'tema_1',
+                'text' : 'Las sumas',
+                'actividades' : {
+                    'act_1' :{
+                        'id' : 'act_1',
+                        'text' : 'La loca de los gatos'
+                    },
+                    'act_2' :{
+                        'id' : 'act_2',
+                        'text' : 'Video explicativo'
+                    },
+                    'act_3' :{
+                        'id' : 'act_3',
+                        'text' : 'Tetris de sumas'
+                    },
+                    'act_4' :{
+                        'id' : 'act_4',
+                        'text' : 'Rush'
+                    }
+                }
+            }
+        }
+    }
+}
+
--- mthauth/models.py
+++ mthauth/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class users(models.Model):
-    nivel = models.IntegerField(default=0)
-    tema = models.CharField(max_length = 30, default="")
-    actividad = models.CharField(max_length= 30, default="")
-    porcentaje = models.IntegerField(default=0)
+    user = models.CharField(max_length = 20)
+    password = models.CharField(max_length = 50)
+    email = models.EmailField()
+class session(models.Model):
+    userto = models.CharField(max_length = 20)
+    ide = models.CharField(max_length = 15)
+    state = models.CharField(max_length = 10)
